c45baa04b199b5d942d87f9638f4c5b4b0504154
roshidhmohammed <roshidhmohammed777@gmail.com>
2025-06-08 20:43:26 +0530

    routes

--- src/app.js
+++ src/app.js
@@ -1,14 +1,28 @@
 const express = require("express")
 
 const app =express()
-
-app.use("/server", (req, res)=>{
-    res.send("Hii from the serve")
+app.get('/user/:userId/:name', (req, res)=>{
+    // console.log(req.query)
+    console.log(req.params)
+    res.send({firstName:"Mohammed", lastName:"Roshidh s"})
 })
+// app.get("/user", (req,res)=>{
+//     res.send("Successfully retrieved the user")
+// })
 
-app.use("/test", (req, res)=>{
-    res.send("Hii from the test")
-})
+// app.post("/user", (req,res)=>{
+//     res.send("Successfully created the user")
+// })
+
+// app.delete("/user", (req,res)=>{
+//     res.send("Successully deleted the user")
+// })
+
+// app.put("/user", (req,res)=>{
+//     res.send("Successully updated the user profile")
+// })
+
+
 
 app.listen(8000,()=>{
     console.log("Server is running on the port 8000")
