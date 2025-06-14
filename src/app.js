53c68ed923009b15c2b7750c7d35b9e32ecc5e3e
roshidhmohammed <roshidhmohammed777@gmail.com>
2025-06-15 01:24:15 +0530

    moved-compare-password-and-jwt-sign-in-user-schema-level

--- src/app.js
+++ src/app.js
@@ -1,32 +1,29 @@
 const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
-const { signUpValidation,
-  loginValidation
- } = require("./utils/validation");
-const bcrypt =require("bcrypt")
-const cookieParser  = require("cookie-parser")
-const json =require("jsonwebtoken");
+const { signUpValidation, loginValidation } = require("./utils/validation");
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+const json = require("jsonwebtoken");
 const { userAuth } = require("./middlewares/auth");
 
 const app = express();
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
 
 app.post("/sign-up", async (req, res) => {
-  
   try {
-    signUpValidation(req)
-    const {password, firstName, lastName, emailId} = req.body
-    const hashedPassword =await bcrypt.hash(password, 1 )
+    signUpValidation(req);
+    const { password, firstName, lastName, emailId } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 1);
     const user = new User({
       firstName,
       lastName,
       emailId,
-      password:hashedPassword
+      password: hashedPassword,
     });
-   
-    const isEmailExist = await User.findOne({ emailId:emailId });
+
+    const isEmailExist = await User.findOne({ emailId: emailId });
     if (isEmailExist) {
       throw new Error("Email Id already exist");
     }
@@ -37,48 +34,47 @@ app.post("/sign-up", async (req, res) => {
   }
 });
 
-
-app.post("/login", async(req, res) =>{
+app.post("/login", async (req, res) => {
   try {
-    const {emailId, password} = req.body
-    loginValidation(req)
-    const user = await User.findOne({emailId})
-    if(!user) {
-      throw new Error("Invalid Login Details")
+    const { emailId, password } = req.body;
+    loginValidation(req);
+    const user = await User.findOne({ emailId });
+    if (!user) {
+      throw new Error("Invalid Login Details");
     }
 
-    const isPasswordValid = await bcrypt.compare(password, user.password)
-    if(!isPasswordValid){
-      throw new Error("Invalid Login Details")
+    const isPasswordValid =await user.validatePassword(password);
+    if (!isPasswordValid) {
+      throw new Error("Invalid Login Details");
     } else {
-      const token =await  json.sign({_id:user?._id}, "Roshidh123@", {expiresIn:"1d"})
-      res.cookie("token", token, {expires:new Date(Date.now() + 24 * 3600000)})
-      res.send("Login Successfull!")
+      const token = await user.getJWT();
+      res.cookie("token", token, {
+        expires: new Date(Date.now() + 24 * 3600000),
+      });
+      res.send("Login Successfull!");
     }
   } catch (error) {
-    res.status(400).send('Error :' +  error);
+    res.status(400).send("Error :" + error);
   }
-})
+});
 
-app.use("/profile", userAuth, async(req, res)=>{
+app.use("/profile", userAuth, async (req, res) => {
   try {
-    const user = req.user
-      res.send(user)
-
-    
+    const user = req.user;
+    res.send(user);
   } catch (error) {
-     res.status(400).send('Error :' +  error);
+    res.status(400).send("Error :" + error);
   }
-})
+});
 
-app.post("/send-connection-request",userAuth, async(req, res, next) =>{
+app.post("/send-connection-request", userAuth, async (req, res, next) => {
   try {
-    const user = req.user
-    res.send(user.firstName + " sent connection request")
+    const user = req.user;
+    res.send(user.firstName + " sent connection request");
   } catch (error) {
-    res.status(400).send('Error :' +  error);
+    res.status(400).send("Error :" + error);
   }
-})
+});
 
 connectDB()
   .then(() => {
--- src/models/user.js
+++ src/models/user.js
@@ -1,69 +1,87 @@
 const mongoose = require("mongoose");
-const validator = require("validator")
+const validator = require("validator");
+const json = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minLength:2,
-    maxLength:20
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 20,
+    },
+    lastName: {
+      type: String,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email is not valid:", value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Your Password is not strong:", value);
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    isSubscribed: {
+      type: Boolean,
+      default: false,
+    },
+    profilePic: {
+      type: String,
+      trim: true,
+      default: null,
+    },
+    skills: {
+      type: [String],
+    },
+    gender: {
+      type: String,
+      trim: true,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
   },
-  lastName: {
-    type: String,
-  },
-  emailId: {
-    type: String,
-    required:true,
-    unique:true,
-    trim: true,
-    lowercase:true,
-    validate(value) {
-     if(! validator.isEmail(value)){
-      throw new Error("Email is not valid:", value)
-     }
-    }
-  },
-  password: {
-    type: String,
-    required:true,
-    trim:true,
-    validate(value){
-      if(!validator.isStrongPassword(value)){
-        throw new Error("Your Password is not strong:", value)
-      }
-    }
-  },
-  age: {
-    type: Number,
-    min:18
-  },
-  isSubscribed:{
-    type: Boolean,
-    default:false
-  },
-  profilePic:{
-    type:String,
-    trim:true,
-    // validate(value){
-    //   if(!validator.isURL(value)){
-    //     throw new Error("Your Photo URL is not valid:", value)
-    //   }
-    // },
-    default:null,
-  },
-  skills:{
-    type:[String]
-  },
-  gender: {
-    type: String,
-    trim:true,
-    validate(value){
-      if(!["male", "female", "others"].includes(value)){
-        throw new Error("Gender data is not valid")
-      }
-    }
-  },
-}, {timestamps:true});
+  { timestamps: true }
+);
+
+userSchema.methods.getJWT = async function () {
+  const user = this;
+  const token = await json.sign({ _id: user?._id }, "Roshidh123@", {
+    expiresIn: "1d",
+  });
+  return token;
+};
+
+userSchema.methods.validatePassword = async function (userInputPassword) {
+  const user = this;
+  const hashedPassword = user?.password;
+  const isPasswordValid = await bcrypt.compare(
+    userInputPassword,
+    hashedPassword
+  );
+  return isPasswordValid;
+};
 
 const User = mongoose.model("User", userSchema);
 
