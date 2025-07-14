ac2ae5007614424e209230ff494bd433e22e045f
roshidhmohammed <roshidhmohammed777@gmail.com>
2025-07-14 20:54:14 +0530

    updating the aws region for production

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,6 +1,6 @@
 const AWS = require("aws-sdk");
 AWS.config.update({
-  region: `${process.env.AWS_REGION}`,
+  region: "ap-south-1",
   credentials: {
     accessKeyId: `${process.env.AWS_ACCESS_KEY}`,
     secretAccessKey: `${process.env.AWS_SECRET_KEY}`,
