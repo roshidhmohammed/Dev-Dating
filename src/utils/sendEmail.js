28fb3a0984e0ca967e8d77e8371acde2d6e7db90
roshidhmohammed <roshidhmohammed777@gmail.com>
2025-07-14 20:49:00 +0530

    updating the aws ses region

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,6 +1,6 @@
 const AWS = require("aws-sdk");
 AWS.config.update({
-  region: process.env.AWS_REGION,
+  region: `${process.env.AWS_REGION}`,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY,
     secretAccessKey: process.env.AWS_SECRET_KEY,
