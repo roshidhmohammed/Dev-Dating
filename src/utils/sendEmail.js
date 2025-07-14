4fd0a4d05fd3a698320ed6f9369f4ce0e79652a8
roshidhmohammed <roshidhmohammed777@gmail.com>
2025-07-14 20:50:45 +0530

    updating aws ses credentials

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -2,8 +2,8 @@ const AWS = require("aws-sdk");
 AWS.config.update({
   region: `${process.env.AWS_REGION}`,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_KEY,
+    accessKeyId: `${process.env.AWS_ACCESS_KEY}`,
+    secretAccessKey: `${process.env.AWS_SECRET_KEY}`,
   },
 });
 
