4829fd60f731e740aa13e7ec1e72a47560cd0eaa
roshidhmohammed <roshidhmohammed777@gmail.com>
2025-07-14 23:04:26 +0530

    updating the aws credentials

--- src/utils/sesClient.js
+++ src/utils/sesClient.js
@@ -5,8 +5,8 @@ const REGION = "ap-south-1";
 const sesClient = new SESClient({
   region: REGION,
   credentials: {
-    accessKeyId: `${process.env.AWS_ACCESS_KEY}`,
-    secretAccessKey: `${process.env.AWS_SECRET_KEY}`,
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
   },
 })
 module.exports = { sesClient }
