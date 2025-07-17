57daadfed48207eca8760542ff090bc2b1db96d1
roshidhmohammed <roshidhmohammed777@gmail.com>
2025-07-18 00:02:08 +0530

    updating the payment verify response bug issue 3

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -84,12 +84,13 @@ paymentRouter.post("/payment/webhook", async(req, res)=>{
   }
 })
 
-paymentRouter.get("/payment/verify", async(req, res)=>{
+paymentRouter.get("/payment/verify",userAuth, async(req, res)=>{
   try {
-    const user = req.user.toJSON()
+    const user = req.user
+    // const userDetails =  await User.find({_id:user._id.to})
     console.log(user)
     // if(user.isPremium){
-      return res.status(200).json({...user})
+       res.status(200).json({user})
     // }
   } catch (error) {
         res.status(500).send(error.message);
--- src/routes/profile.js
+++ src/routes/profile.js
@@ -8,9 +8,9 @@ const profileRouter = express.Router();
 
 profileRouter.use("/profile/view", userAuth, async (req, res) => {
   try {
-    const user = req.user.toJSON();
+    const user = req.user;
     console.log(user)
-    res.status(200).json({...user});
+    res.status(200).send(user);
   } catch (error) {
     res.status(400).send("Error :" + error)
   }
