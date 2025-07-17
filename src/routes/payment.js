029687b73d9a1a5c7460956ecb606f9d9f2a990d
roshidhmohammed <roshidhmohammed777@gmail.com>
2025-07-18 00:39:35 +0530

    razorpay env updated

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -4,9 +4,10 @@ const instance = require("../utils/razorpay");
 const membershipAmount = require("../utils/constants");
 const paymentRouter = express.Router();
 const Payment = require("../models/payment");
-const {validateWebhookSignature} = require('razorpay/dist/utils/razorpay-utils');
+const {
+  validateWebhookSignature,
+} = require("razorpay/dist/utils/razorpay-utils");
 const User = require("../models/user");
-
 
 paymentRouter.post("/payment/create", userAuth, async (req, res) => {
   try {
@@ -37,68 +38,73 @@ paymentRouter.post("/payment/create", userAuth, async (req, res) => {
 
     const savedPayment = await payment.save();
     res.status(201).json({
-        ...savedPayment.toJSON(), keyId:process.env.RAZORPAY_KEY_ID
-    })
+      ...savedPayment.toJSON(),
+      keyId: process.env.RAZORPAY_KEY_ID,
+    });
   } catch (error) {
     res.status(400).send(error.message);
   }
 });
 
-paymentRouter.post("/payment/webhook", async(req, res)=>{
+paymentRouter.post("/payment/webhook", async (req, res) => {
   try {
-    const webhookSignature = req.headers('X-Razorpay-Signature')
-   const isWebhookValid =  validateWebhookSignature(JSON.stringify(req.body), webhookSignature, process.env.RAZORPAY_WEBHOOK_SECRET)
+    const webhookSignature = req.headers("X-Razorpay-Signature");
+    const isWebhookValid = validateWebhookSignature(
+      JSON.stringify(req.body),
+      webhookSignature,
+      process.env.RAZORPAY_WEBHOOK_SECRET
+    );
 
-   if(!isWebhookValid){
-    return res.status(400).json({
-      message:"Payment staus is not accessible"
-    })
+    if (!isWebhookValid) {
+      return res.status(400).json({
+        message: "Payment staus is not accessible",
+      });
+    }
 
-  }
+    console.log("webhook", isWebhookValid);
+    const paymentDetails = req.body.payload.payment.entity;
 
-  console.log("webhook", isWebhookValid)
-  const paymentDetails = req.body.payload.payment.entity
+    console.log("payment", paymentDetails);
 
-  console.log("payment", paymentDetails)
+    const updatePayment = await Payment.findOne({
+      orderId: paymentDetails.order_id,
+    });
+    updatePayment.status = paymentDetails.status;
+    await updatePayment.save();
+    console.log("Payment saved");
 
-  const updatePayment = await Payment.findOne({orderId:paymentDetails.order_id})
-  updatePayment.status = paymentDetails.status
-  await updatePayment.save()
-  console.log("Payment saved");
+    const updateUser = await User.findOne({ _id: paymentDetails.userId });
+    updateUser.isPremium = true;
+    updateUser.membershipType = paymentDetails.notes.membershipType;
+    console.log("updated user");
 
-  const updateUser = await User.findOne({_id:paymentDetails.userId})
-  updateUser.isPremium = true
-  updateUser.membershipType = paymentDetails.notes.membershipType
-  console.log("updated user")
+    await updateUser.save();
 
-  await updateUser.save()
+    //  if(req.body.event === "payment.captured") {
 
+    //  }
 
-  //  if(req.body.event === "payment.captured") {
+    //  if(req.body.event === "payment.failed") {
 
-  //  }
+    //  }
 
-  //  if(req.body.event === "payment.failed") {
-
-  //  }
-
-  return res.status(200).json({message:"Webhook received successfully"})
+    return res.status(200).json({ message: "Webhook received successfully" });
   } catch (error) {
     res.status(500).send(error.message);
   }
-})
+});
 
-paymentRouter.get("/payment/verify",userAuth, async(req, res)=>{
+paymentRouter.get("/payment/verify", userAuth, async (req, res) => {
   try {
-    const user = req.user
+    const user = req.user;
     // const userDetails =  await User.find({_id:user._id.to})
-    console.log(user)
+    console.log(user);
     // if(user.isPremium){
-       res.status(200).json({user})
+    res.status(200).json({ user });
     // }
   } catch (error) {
-        res.status(500).send(error.message);
+    res.status(500).send(error.message);
   }
-})
+});
 
 module.exports = paymentRouter;
