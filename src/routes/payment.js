fe84321493e4404235d564387b38ec165f165696
roshidhmohammed <roshidhmohammed777@gmail.com>
2025-07-17 23:44:56 +0530

    updating the payment verify bug

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -86,10 +86,10 @@ paymentRouter.post("/payment/webhook", async(req, res)=>{
 
 paymentRouter.get("/payment/verify", async(req, res)=>{
   try {
-    const user = req.user.toJSON()
-    if(user.isPremium){
+    const user = req.user
+    // if(user.isPremium){
       return res.status(200).json({user})
-    }
+    // }
   } catch (error) {
         res.status(500).send(error.message);
   }
